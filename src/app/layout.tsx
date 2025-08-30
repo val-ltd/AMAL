7321fa6ed5a01e4482cb19b34d5e375743e64719
val enterprise <val.enterprise.limited@gmail.com>
2025-08-30 17:56:41 +0000

    Could you change the style to be more arabian but keep the language

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Scheherazade+New&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased">
         <AppShell>{children}</AppShell>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,8 +10,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['Inter', 'sans-serif'],
-        headline: ['Inter', 'sans-serif'],
+        body: ['Scheherazade New', 'serif'],
+        headline: ['Scheherazade New', 'serif'],
         code: ['monospace'],
       },
       colors: {
