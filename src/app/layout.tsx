e91faab3f656988e42a0c98bc96d8f059022a9c9
val enterprise <val.enterprise.limited@gmail.com>
2025-09-01 07:39:44 +0000

    now I want this app to look like native, you can either make it PWA or a

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import { AuthProvider } from '@/hooks/use-auth';
 export const metadata: Metadata = {
   title: 'BudgetFlow',
   description: 'Aplikasi permintaan dan persetujuan anggaran profesional.',
+  manifest: '/manifest.json',
 };
 
 export default function RootLayout({
@@ -20,6 +21,8 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Scheherazade+New&display=swap" rel="stylesheet" />
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+        <meta name="theme-color" content="#12745a" />
       </head>
       <body className="font-body antialiased">
         <AuthProvider>
