4cfb55cbd17427ca828d2488647aeffc919c8d6f
val enterprise <val.enterprise.limited@gmail.com>
2025-09-11 14:50:08 +0000

    i've setup the secret in the .env, why?

--- next.config.ts
+++ next.config.ts
@@ -44,6 +44,10 @@ const nextConfig: NextConfig = {
       }
     ],
   },
+  env: {
+    NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+    NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET: process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET,
+  }
 };
 
 export default withPWA(nextConfig);
